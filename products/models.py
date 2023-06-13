d2bc70b1562583b241fe0097ff38d84c0b68ad73
deima24 <12deima24@gmail.com>
2023-06-13 23:20:41 +0000

    added meat free products

--- products/models.py
+++ products/models.py
@@ -13,13 +13,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.TextField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     is_meat_free = models.BooleanField(default=False, null=True, blank=True)
     ingredients = models.TextField()
     image = models.ImageField(null=True, blank=True)
-    nutritional_information = models.TextField()
-
+    
 
     def __str__(self):
         return self.name
