5399d3ee4e1ac473369256a2903ea22fcdd91f87
deima24 <12deima24@gmail.com>
2023-06-16 23:02:29 +0000

    instaled products and categories fromfixtures folder

--- products/models.py
+++ products/models.py
@@ -3,8 +3,15 @@ from django.db import models
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=60)
     image = models.ImageField(null=True, blank=True)
+    
+    class Meta:
+        """
+        Ensure correct plural of Category
+        in admin UI
+        """
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
