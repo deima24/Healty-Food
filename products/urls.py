7140feb762f1d8e5b3f4b3673cd24f8248afc529
deima24 <12deima24@gmail.com>
2023-06-21 19:56:57 +0000

    created product detail html

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.all_categories, name='categories'),
     path('products/', views.products, name="products"),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
