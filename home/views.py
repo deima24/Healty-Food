238fa008e62dc931a0ae8e11540f6b84d5016b7a
deima24 <12deima24@gmail.com>
2023-06-23 22:46:37 +0000

    added about.html template and costumize it

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return index page """
     
     return render(request, 'home/index.html')
+
+def about(request):
+    """
+    renders about page
+    """
+    return render(request, 'home/about.html')
