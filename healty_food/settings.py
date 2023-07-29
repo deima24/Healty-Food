ec0ff5ed76317c0d5e8877e1b0c4c9b50c4e0d1b
deima24 <12deima24@gmail.com>
2023-07-29 23:00:51 +0000

    migrate to database

--- healty_food/settings.py
+++ healty_food/settings.py
@@ -144,9 +144,9 @@ if "DATABASE_URL" in os.environ:
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+       "default": {
+           "ENGINE": "django.db.backends.sqlite3",
+           "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 # Password validation
